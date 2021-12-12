8c2ac7be6d2c48f52ede71210380ea0c152dbf78
lntaivn <94955437+lntaivn@users.noreply.github.com>
2021-12-12 15:28:05 +0700

    Add files via upload

---
+++ j2/admin/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <title>Shop Điện Thoại</title>
+</head>
+<body>
+    <div class="sidebar">
+        <div class="sidebar-brand">
+            <h2> <span class="lab la-accusoft"></span>
+             Shop điện thoại
+            </h2>
+        </div>
+        <div class="sidebar-menu">
+            <ul>
+                <li>
+                    <a href="" class="active">
+                        <span class="las la-igloo"></span>
+                        <span>Trang chủ</span>
+                    </a>
+                </li><li>
+                    <a href="">
+                        <span class="las la-users"></span>
+                        <span>Quản lý NSX</span>
+                    </a>
+                </li><li>
+                    <a href="">
+                        <span class="las la-clipboard-list"></span>
+                        <span>Tạo sản phẩm</span>
+                    </a>
+                </li><li>
+                    <a href="">
+                        <span class="las la-shopping-bag"></span>
+                        <span>Đơn hàng</span>
+                    </a>
+                </li><li>
+                    <a href="">
+                        <span class="las la-receipt"></span>
+                        <span>Duyệt đơn</span>
+                    </a>
+                </li><li>
+                    <a href="">
+                        <span class="las la-user-circle"></span>
+                        <span>Tài khoảng</span>
+                    </a>
+                </li><li>
+                    <a href="">
+                        <span class="las la-clipboard-list"></span>
+                        <span>Tasks</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="main-content">
+        <header>
+            <h2>
+                <label for="">
+                    <span class="las la-bars"></span>
+                </label>
+
+                Trang chủ
+
+            </h2>
+
+            <div class="search-wrapper">
+                <span class="las la-search"></span>
+                <input type="search" placeholder="search here">
+            </div>
+
+            <div class="user-wrapper">
+                <img src="https://helena-jsc.com/wp-content/uploads/2021/07/e0nk-pdxeaa1fy6.jpg" width="40px" height="40px" alt="">
+                <div>
+                    <h4> Tài đẹp trai </h4>
+                    <small>admin</small>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="cards">
+                <div class="card-single">
+                    <h1>54</h1>
+                    <span>Nhà sản xuất</span>
+                </div>
+                <div>
+                    <span class="las la-users"></span>
+                </div>
+                <div class="card-single">
+                    <h1>76</h1>
+                    <span>sản phẩm</span>
+                </div>
+                <div>
+                    <span class="las la-clipboard-list"></span>
+                </div>
+                <div class="card-single">
+                    <h1>100</h1>
+                    <span>Đơn hàng</span>
+                </div>
+                <div>
+                    <span class="las la-shopping-bag"></span>
+                </div>
+                <div class="card-single">
+                    <h1>$6</h1>
+                    <span>Giá</span>
+                </div>
+                <div>
+                    <span class="las la-google-wallet"></span>
+                </div>
+            </div>
+        </main>
+    </div>
+</body>
+</html>
